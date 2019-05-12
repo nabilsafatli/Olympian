ac80de04374855c32d307afeb610a8848e086474
Nabil Safatli <safatli.nabil@gmail.com>
2019-05-12 18:07:18 +0200

    Created Cocoa Touch framework for Olympian Player

---
+++ OlympianPlayer/OlympianPlayer/OlympianPlayer.h
@@ -0,0 +1,19 @@
+//
+//  OlympianPlayer.h
+//  OlympianPlayer
+//
+//  Created by Nabil Safatli on 2019-05-12.
+//  Copyright © 2019 Olympian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OlympianPlayer.
+FOUNDATION_EXPORT double OlympianPlayerVersionNumber;
+
+//! Project version string for OlympianPlayer.
+FOUNDATION_EXPORT const unsigned char OlympianPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OlympianPlayer/PublicHeader.h>
+
+
