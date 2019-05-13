69a3f936a1894233cc55761027899225c155cc2a
nabil.safatli <safatli.nabil@gmail.com>
2019-05-13 23:22:03 +0200

    Added autoplay property in Olympian Player

--- Source/OlympianPlayer/OlympianPlayer/OlympianPlayer.swift
+++ Source/OlympianPlayer/OlympianPlayer/OlympianPlayer.swift
@@ -12,6 +12,7 @@ import AVKit
 public class OlympianPlayer {
     private var avPlayer: AVPlayer!
     var url: URL?
+    var autoPlay: Bool = false
 
     public init(with container: UIViewController, url: URL? = nil) {
         guard let url = url else { return }
@@ -19,7 +20,7 @@ public class OlympianPlayer {
         let playerViewController = AVPlayerViewController()
         let playerFrame = CGRect(x: 0, y: 0, width: container.view.frame.width, height: container.view.frame.height)
         playerViewController.player = avPlayer
-        avPlayer.rate = 1
+        avPlayer.rate = autoPlay ? 1 : 0
         playerViewController.view.frame = playerFrame
 
         container.addChild(playerViewController)
