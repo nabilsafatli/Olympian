4ce537c44a80fa4a6a9a8d2f050e6a2b4d0457bd
nabil.safatli <safatli.nabil@gmail.com>
2019-05-26 12:44:28 +0200

    Evolved the Media Control to have an interface and an array of UI elements

--- Source/OlympianPlayer/OlympianPlayer/Controls/MediaControl.swift
+++ Source/OlympianPlayer/OlympianPlayer/Controls/MediaControl.swift
@@ -8,23 +8,58 @@
 
 import Foundation
 
-class MediaControl: UIView {
+class MediaControl: UIView, MediaControlling {
+    var status: PlayerStatus = .none
+    var player: OlympianPlayerProtocol?
+    var elements = [MediaControlElement]()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setupView()
     }
 
-    func setupView() {
+    func setup() {
         guard let frame = superview?.frame else { return }
 
         self.frame = frame
         let playPauseButton = PlayPauseButton()
+        playPauseButton.mediaControl = self
+        elements.append(playPauseButton)
         addSubview(playPauseButton)
         playPauseButton.setupView()
     }
+
+    func play() {
+        status = .playing
+        player?.play()
+        elements.forEach { $0.refresh() }
+    }
+
+    func pause() {
+        status = .paused
+        player?.pause()
+        elements.forEach { $0.refresh() }
+    }
+}
+
+protocol MediaControlling: UIView {
+    var player: OlympianPlayerProtocol? { get set }
+    var status: PlayerStatus { get set }
+
+    func setup()
+    func play()
+    func pause()
+}
+
+enum PlayerStatus {
+    case none
+    case playing
+    case paused
+}
+
+protocol MediaControlElement {
+    func refresh()
 }
